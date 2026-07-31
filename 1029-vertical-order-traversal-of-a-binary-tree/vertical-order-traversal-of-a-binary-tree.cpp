aeed9d809ac821fbfbe02c414e39fc3242850caa
AMAN KUMAR <pprincearya11@gmail.com>
2026-07-31 19:24:44 +0530

    Time: 0 ms (100.00%) | Memory: 15.4 MB (91.15%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,38 +1,31 @@
 class Solution {
 public:
+    vector<tuple<int, int, int>> nodes;
+
+    void dfs(TreeNode* root, int row, int col) {
+        if (!root) return;
+
+        nodes.push_back({col, row, root->val});
+
+        dfs(root->left, row + 1, col - 1);
+        dfs(root->right, row + 1, col + 1);
+    }
+
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map<int, map<int, multiset<int>>> mp;
-        queue<pair<TreeNode*, pair<int, int>>> q;
+        dfs(root, 0, 0);
 
-        q.push({root, {0, 0}});
-
-        while (!q.empty()) {
-            auto it = q.front();
-            q.pop();
-
-            TreeNode* node = it.first;
-            int x = it.second.first;
-            int y = it.second.second;
-
-            mp[x][y].insert(node->val);
-
-            if (node->left)
-                q.push({node->left, {x - 1, y + 1}});
-
-            if (node->right)
-                q.push({node->right, {x + 1, y + 1}});
-        }
+        sort(nodes.begin(), nodes.end());
 
         vector<vector<int>> ans;
 
-        for (auto &col : mp) {
-            vector<int> temp;
+        int prevCol = INT_MIN;
 
-            for (auto &row : col.second) {
-                temp.insert(temp.end(), row.second.begin(), row.second.end());
+        for (auto &[col, row, val] : nodes) {
+            if (col != prevCol) {
+                ans.push_back({});
+                prevCol = col;
             }
-
-            ans.push_back(temp);
+            ans.back().push_back(val);
         }
 
         return ans;
