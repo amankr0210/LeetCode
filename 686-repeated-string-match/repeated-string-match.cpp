51e455bf55244c753b4dbf95c111f32e12bf901f
AMAN KUMAR <pprincearya11@gmail.com>
2026-08-16 22:06:37 +0530

    Time: 3 ms (76.04%) | Memory: 10.2 MB (29.34%) - LeetSync

--- 686-repeated-string-match/repeated-string-match.cpp
+++ 686-repeated-string-match/repeated-string-match.cpp
@@ -1,30 +1,53 @@
 class Solution {
     public:
     int repeatedStringMatch(string a, string b) {
-        string s = "";
-        int count = 0;
+        int n = a.size(), m = b.size();
 
-        while (s.length() < b.length()) {
-            s += a;
-            count++;
-        }
+        string text;
+        int times = (m + n - 1) / n;
 
-        if (s.find(b) != string::npos) {
-            return count;
-        }
+        text.reserve((times + 1) * n);
 
-        s += a;
-        count++;
-        if (s.find(b) != string::npos) {
-            return count;
-        }
+        for (int i = 0; i < times; ++i)
+        text += a;
 
-        s += a;
-        count++;
-        if (s.find(b) != string::npos) {
-            return count;
-        }
+        if (kmp(text, b))
+        return times;
+
+        text += a;
+
+        if (kmp(text, b))
+        return times + 1;
 
         return -1;
+    }
+
+    private:
+    bool kmp(const string& text, const string& pattern) {
+        int m = pattern.size();
+        vector<int> lps(m, 0);
+
+        for (int i = 1, j = 0; i < m; ++i) {
+            while (j > 0 && pattern[i] != pattern[j])
+            j = lps[j - 1];
+
+            if (pattern[i] == pattern[j])
+            ++j;
+
+            lps[i] = j;
+        }
+
+        for (int i = 0, j = 0; i < text.size(); ++i) {
+            while (j > 0 && text[i] != pattern[j])
+            j = lps[j - 1];
+
+            if (text[i] == pattern[j])
+            ++j;
+
+            if (j == m)
+            return true;
+        }
+
+        return false;
     }
 };
