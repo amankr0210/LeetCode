a4d2e89efa628b6c609d6f640105f8e926699678
AMAN KUMAR <pprincearya11@gmail.com>
2026-08-16 22:03:04 +0530

    Time: 11 ms (16.79%) | Memory: 9.9 MB (45.89%) - LeetSync

--- 686-repeated-string-match/repeated-string-match.cpp
+++ 686-repeated-string-match/repeated-string-match.cpp
@@ -4,25 +4,21 @@ class Solution {
         string s = "";
         int count = 0;
 
-        // Repeat 'a' until its length is at least equal to 'b'
         while (s.length() < b.length()) {
             s += a;
             count++;
         }
 
-        // Check if 'b' is a substring
         if (s.find(b) != string::npos) {
             return count;
         }
 
-        // Check with one more repetition (to handle offset/alignment)
         s += a;
         count++;
         if (s.find(b) != string::npos) {
             return count;
         }
 
-        // Check with a second extra repetition
         s += a;
         count++;
         if (s.find(b) != string::npos) {
